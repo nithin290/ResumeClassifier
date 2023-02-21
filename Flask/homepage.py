066886e81da537261a56ef5463fcc1a8fbd1c612
ShyamB123 <shyamer3@gmail.com>
2023-02-21 19:50:23 +0530

    test commit

--- Flask/homepage.py
+++ Flask/homepage.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Defining the home page of our site
-@app.route("/test")  # this sets the route to this page
+@app.route("/")  # this sets the route to this page
 def home():
 	return render_template("index.html")
 
